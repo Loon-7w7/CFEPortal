dfc8ab9a4f134f74ecc23c14cd704d5cbc174ab6
Luis Ku <dual.luis.ku@gmail.com>
2023-05-15 23:09:05 -0600

    paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalcMaterialComponent } from './page/components/calc.material/calc.material.component';
+import { DevicesComponent } from './page/components/devices/devices.component';
+import { ConsultationComponent } from './page/components/consultation/consultation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',
+    redirectTo: '/CalcMaterial', 
+    pathMatch: 'full'
+  },
+  {
+    path:'CalcMaterial',
+    component: CalcMaterialComponent
+  },
+  {
+    path:'Devices',
+    component: DevicesComponent
+  },
+  {
+    path:'Consultation',
+    component: ConsultationComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/page/components/calc.material/calc.material.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cfe-calc.material',
+  templateUrl: './calc.material.component.html',
+  styleUrls: ['./calc.material.component.css']
+})
+export class CalcMaterialComponent {
+
+}
