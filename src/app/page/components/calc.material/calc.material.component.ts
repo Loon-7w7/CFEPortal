17d84a3ac150b2bc43a98842952c5c3f58aaa409
Luis Ku <dual.luis.ku@gmail.com>
2023-05-16 12:23:04 -0600

    alertas

--- src/app/page/components/calc.material/calc.material.component.ts
+++ src/app/page/components/calc.material/calc.material.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { CalcInfoForm } from 'src/app/core/forms.models/Calc.Info.Form.model';
 
 @Component({
@@ -12,7 +13,7 @@ export class CalcMaterialComponent {
   calcInfo: CalcInfoForm;
   showTableData = false;
   isFormDisabled = false;
-  constructor() {
+  constructor(private toastr: ToastrService) {
     this.calcInfo = {
       number: null,
       year: null,
@@ -30,53 +31,47 @@ export class CalcMaterialComponent {
       this.showTableData = true;
       this.isFormDisabled = true;
     }
+    else {
+      this.toastr.error(this.getMessageAlerteErrorForm(), 'Los siguinetes datos son requeridos:')
+    }
   }
-  formValidator():boolean 
-  {
+  formValidator(): boolean {
     let respose = true;
-    if(
-        this.calcInfo.number == null || 
-        this.calcInfo.name == null || 
-        this.calcInfo.description == null ||
-        this.calcInfo.year == null ||
-        this.calcInfo.voltage == null ||
-        this.calcInfo.semiisolated == null ||
-        this.calcInfo.heavy == null
-        ){
-          respose = false
-        }
-      return respose;
+    if (
+      this.calcInfo.number == null ||
+      this.calcInfo.name == null ||
+      this.calcInfo.description == null ||
+      this.calcInfo.year == null ||
+      this.calcInfo.voltage == null ||
+      this.calcInfo.semiisolated == null ||
+      this.calcInfo.heavy == null
+    ) {
+      respose = false
+    }
+    return respose;
   }
-  getMessageAlerteErrorForm(): string
-  {
-    let message = "Los siguinetes datos son requeridos:"
-    if(this.calcInfo.number == null)
-    {
-      message += ' Numero'
+  getMessageAlerteErrorForm(): string {
+    let message = '';
+    if (this.calcInfo.number == null) {
+      message += '-Numero-'
     }
-    if(this.calcInfo.name == null)
-    {
-      message += ' Nombre'
+    if (this.calcInfo.name == null) {
+      message += '-Nombre-'
     }
-    if(this.calcInfo.description == null )
-    {
-      message += ' Descripcion'
+    if (this.calcInfo.description == null) {
+      message += '-Descripcion-'
     }
-    if(this.calcInfo.year == null )
-    {
-      message += ' Año'
+    if (this.calcInfo.year == null) {
+      message += '-Año-'
     }
-    if(this.calcInfo.voltage == null)
-    {
-      message += ' Voltaje'
+    if (this.calcInfo.voltage == null) {
+      message += '-Voltaje-'
     }
-    if(this.calcInfo.semiisolated == null)
-    {
-      message += ' Semi aislado'
+    if (this.calcInfo.semiisolated == null) {
+      message += '-Semi aislado-'
     }
-    if(this.calcInfo.heavy == null)
-    {
-      message += ' Pesado'
+    if (this.calcInfo.heavy == null) {
+      message += '-Pesado-'
     }
     return message;
   }
