1fb1a558c8bbe5ee60ca9d01ccec60c643ed8b46
Luis Ku <dual.luis.ku@gmail.com>
2023-06-04 00:22:15 -0600

    Servicio para eliminar

--- src/app/services/implementation/Material-sevices.ts
+++ src/app/services/implementation/Material-sevices.ts
@@ -5,47 +5,62 @@ import { Injectable } from "@angular/core";
 import { CreateMaterrialFrom } from "src/app/core/forms.models/CreateMaterial";
 
 @Injectable()
-export class MaterialServices{
+export class MaterialServices {
     private loginUrl = 'api/Material'
     constructor(private http: HttpClient) { }
     getMaterials(): Observable<Material[]> {
         const headers = new HttpHeaders().set('Content-Type', 'application/json');
         return this.http.get<Material[]>(this.loginUrl, { headers })
-        .pipe(
-            map((reponse: Material[]) => {
-                
-                return reponse;
-                  
-            }),
-            catchError(() => {return []})
-        );
+            .pipe(
+                map((reponse: Material[]) => {
+
+                    return reponse;
+
+                }),
+                catchError(() => { return [] })
+            );
     }
-    addMateria(datos:CreateMaterrialFrom):Observable<boolean>
+    addMateria(datos: CreateMaterrialFrom): Observable<boolean> {
+        const headers = new HttpHeaders().set('Content-Type', 'application/json');
+        return this.http.post(this.loginUrl + '/create', datos, { headers })
+            .pipe(
+                map((reponse: any) => {
+                    console.log(reponse);
+                    if (reponse.status == 200) {
+                        return true;
+                    }
+                    else {
+                        return false;
+                    }
+                }),
+                catchError((error: HttpErrorResponse) => {
+                    if (error.status == 200) {
+                        return of(true)
+                    }
+                    else {
+                        return of(false)
+                    }
+                })
+            );
+    }
+    delete(id: string):Observable<boolean>
     {
         const headers = new HttpHeaders().set('Content-Type', 'application/json');
-        return this.http.post(this.loginUrl+'/create', datos, {headers})
-        .pipe(
-            map((reponse: any) => {
-                console.log(reponse);
-                if(reponse.status == 200){
-                    return true;
-                }
-                else
+        return this.http.delete(this.loginUrl+"/Delete"+id, {headers})
+            .pipe(
+                map((Response: any) => 
                 {
-                    return false;
-                }
-            }),
-            catchError((error: HttpErrorResponse) => 
-            {
-               if(error.status == 200)
-               {
-                return of(true)
-               }
-               else
-               {
-                return of(false)
-               }
-            })
-        );
+                    return true;
+                }),
+                catchError((error: HttpErrorResponse) => {
+                    if (error.status == 200) {
+                        return of(true)
+                    }
+                    else {
+                        return of(false)
+                    }
+                })
+            )
     }
+
 }
